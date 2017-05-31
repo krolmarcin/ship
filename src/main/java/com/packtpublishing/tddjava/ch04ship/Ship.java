12f275306cdaff9196a9e0840dc28d1c70d6fcc3
Marcin <szeryfmk@gmail.com>
2017-05-31 11:40:32 +0200

    spec & ship add

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,4 +2,30 @@ package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
 
+    private final Location location;
+
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public boolean moveForward() {
+        return location.forward();
+    }
+
+    public boolean moveBackward() {
+        return location.backward();
+    }
+
+    public void turnLeft() {
+        location.turnLeft();
+    }
+
+    public void turnRight() {
+        location.turnRight();
+    }
+
 }
